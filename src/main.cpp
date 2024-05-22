7bdbadd109317fe05eecd2261fc8a3d569c088a9
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2024-05-22 19:43:23 +0800

    Init

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+﻿DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+{
+#ifndef NDEBUG
+	while (!IsDebuggerPresent()) { Sleep(100); }
+#endif
+
+	DKUtil::Logger::Init(Plugin::NAME, REL::Module::get().version().string());
+
+	//REL::Module::reset();
+	SKSE::Init(a_skse);
+	
+	INFO("{} v{} loaded", Plugin::NAME, Plugin::Version);
+
+	// do stuff
+
+
+	return true;
+}
